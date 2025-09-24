686320d69325a42861eddf1f248f444a8cebeee6
ESET0134 <shruti.singhania@esyasoft.com>
2025-09-24 17:42:06 +0530

    RabbitMQ changes

---
+++ Understanding-Rabbit-MQ/Producer/Program.cs
@@ -0,0 +1,20 @@
+﻿using Producer;
+using Producer.Exchanges;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory() { HostName = "localhost", VirtualHost = "prod", UserName = "guest", Password = "guest" };
+
+DirectExchange directExchange = new DirectExchange(factory);
+
+Console.WriteLine("Enter messages to send (type 'exit' to quit):");
+
+while (true)
+{
+    string? input = Console.ReadLine();
+    if (string.IsNullOrWhiteSpace(input)) continue;
+    if (input.ToLower() == "exit") break;
+
+    await directExchange.SendMessage(input);
+
+
+}
